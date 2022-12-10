aec3647fe2b1eb2efcbbb56237098d710d68ea25
David M. Chen <chen7david@me.com>
2022-12-11 07:13:09 +0800

    added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,110 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import {
+  Button,
+  DatePicker,
+  Layout,
+  Menu,
+  Row,
+  Col,
+  Space,
+  Alert,
+  Card,
+  Divider,
+} from "antd";
+const { Header, Footer, Content, Sider } = Layout;
+import Icon, {
+  HomeOutlined,
+  UserOutlined,
+  ShopOutlined,
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <Layout style={{ height: "100vh" }}>
+        <Header
+          style={{ background: "white" }}
+          className="site-layout-background"
+        >
+          <div className="logo" />
+          {collapsed ? (
+            <MenuUnfoldOutlined onClick={() => setCollapsed(!collapsed)} />
+          ) : (
+            <CloseOutlined onClick={() => setCollapsed(!collapsed)} />
+          )}
+        </Header>
+        <Layout>
+          <Sider
+            trigger={null}
+            collapsible
+            collapsed={collapsed}
+            breakpoint="xs"
+            collapsedWidth="80"
+            theme="light"
+          >
+            <Menu
+              mode="inline"
+              items={[
+                { key: "home", icon: <HomeOutlined />, label: "Home" },
+                { key: "profile", icon: <UserOutlined />, label: "Profile" },
+                { key: "products", icon: <ShopOutlined />, label: "Products" },
+                {
+                  key: "locations",
+                  icon: <MenuFoldOutlined />,
+                  label: "Locations",
+                },
+                {
+                  key: "settings",
+                  icon: <MenuUnfoldOutlined />,
+                  label: "Settings",
+                },
+              ]}
+            />
+          </Sider>
+          <Content style={{ padding: 5 }}>
+            <Alert
+              message="Welcome back!"
+              type="success"
+              showIcon
+              style={{ margin: "10px" }}
+            />
+            <Divider orientation="left">Welcome Back</Divider>
+            <Row gutter={16} align="top" justify="center">
+              <Col
+                xs={24}
+                xl={8}
+                span={6}
+                flex="number"
+                style={{ minHeight: "40vh", background: "#90EE90" }}
+              >
+                col-1
+              </Col>
+              <Col
+                xs={24}
+                xl={8}
+                style={{ minHeight: "40vh", background: "#ADD8E6" }}
+              >
+                col-2
+              </Col>
+              <Col
+                xs={24}
+                xl={8}
+                style={{ minHeight: "40vh", background: "#90EE90" }}
+              >
+                col-3
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+      </Layout>
+    </>
+  );
 }
 
-export default App
+export default App;
